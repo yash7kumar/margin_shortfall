822c3c140205d9484ca41d7cab5ae68bdbe9b0a6
Yash Kumar <yash.kumar@groww.in>
2025-02-03 03:34:34 +0530

    Fixed unused state setters in MarginShortfallCalculator.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import MarginShortfallCalculator from "../components/MarginShortfallCalculator";
 export default function Home() {
   return (
     <div>
-      <h1>Margin Shortfall Calculator</h1>
+     
       <MarginShortfallCalculator />
     </div>
   );
