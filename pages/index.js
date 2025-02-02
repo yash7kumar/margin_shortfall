17334c4202efac7d4213a2a9a4be15f4c7325689
Yash Kumar <yash.kumar@groww.in>
2025-02-03 03:10:08 +0530

    Fixing deployment issues

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import MarginShortfallCalculator from "../components/MarginShortfallCalculator";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Margin Shortfall Calculator</h1>
+      <MarginShortfallCalculator />
+    </div>
+  );
+}
